70b891f28d7127483656f83c0f255520a64bd1c6
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2024-08-03 14:37:34 +0530

    fix: navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-         <h3 className="mx-2 w-10">Pooja Paul</h3>
+         <h3 className="mx-2 w-18">Pooja Paul</h3>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
